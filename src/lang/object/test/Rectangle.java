6446bfa9856b13c00c023a89aceb8b998b1a76f8
Pangpanghoon <lostter0813@gmail.com>
2025-04-03 13:14:14 +0900

    2025.04.03 추가

---
+++ src/lang/object/test/Rectangle.java
@@ -0,0 +1,29 @@
+package lang.object.test;
+
+import java.util.Objects;
+
+public class Rectangle {
+
+    private int height;
+    private int width;
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Rectangle rectangle = (Rectangle) object;
+        return height == rectangle.height && width == rectangle.width;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "height=" + height +
+                ", width=" + width +
+                '}';
+    }
+}
